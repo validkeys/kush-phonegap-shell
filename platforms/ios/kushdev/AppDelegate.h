aaa95584bab9bf8448f89b1e00bd36ac31c15a48
Kyle Davis <validkeys@gmail.com>
2013-10-09 18:52:26 -0400

    more

---
+++ platforms/ios/kushdev/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kushdev
+//
+//  Created by Kyle Davis on 2013-09-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ platforms/ios/kushdevTests/kushdevTests.m
@@ -0,0 +1,34 @@
+//
+//  kushdevTests.m
+//  kushdevTests
+//
+//  Created by Kyle Davis on 2013-09-29.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kushdevTests : XCTestCase
+
+@end
+
+@implementation kushdevTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
